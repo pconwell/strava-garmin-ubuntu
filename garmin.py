ff38587224878bd25cb9399588cdd3093f8fa1ec
pconwell (home) <patrick.conwell@gmail.com>
2017-12-20 06:08:33 -0600

    major update

--- garmin.py
+++ garmin.py
@@ -2,76 +2,72 @@ import os
 import datetime
 import configparser
 import requests
+import webbrowser
 
-# Read authentication from config file
+import webserver
+
+# Read authentication from config file and some intial setup
 config = configparser.ConfigParser()
 config.read(['./config.ini'])
 
-strava_key = config['AUTH']['strava_key']
-strava_token = config['AUTH']['strava_token']
+strava_secret = config['USER']['client_secret']
+strava_cid = config['USER']['client_id']
+strava_token = config['Authentication']['token']
 
-# 
 user = os.getlogin()
 fpath = "/media/" + user + "/GARMIN/GARMIN/ACTIVITY"
 
-activity = []
+# Read activities currently on garmin device
+# Each activity is initially marked with "no" meaning it has not yet been
+# uploaded. A further bit down, we will actually check if it's been uploaded
+# then change "no" to "yes" as necessary.
+garmin_activity = []
 for i in os.listdir(fpath):
-    activity.append([i, datetime.datetime.utcfromtimestamp(os.path.getmtime(fpath + f"/{i}"))])
+    garmin_activity.append([i, datetime.datetime.utcfromtimestamp(os.path.getmtime(fpath + f"/{i}")), "no"])
+garmin_activity.sort(key=lambda x: x[1])
 
-activity.sort(key=lambda x: x[1])
+# Check if auth token exists. If not, ask for authentication and generate one
+# This section needs to be finished, it's only about half done...
+if strava_token == "":
+    webbrowser.open_new_tab('https://www.strava.com/oauth/authorize?client_id=22111&response_type=code&redirect_uri=http://localhost:8000&approval_prompt=force&scope=view_private,write')
+    webserver.run()
 
-# # create permenant record of all activities
-# db = sqlite3.connect('activity.sql')
-# c = db.cursor()
-#
-# # Check if table 'activity' exists
-# c.execute(''' SELECT count(*) FROM sqlite_master WHERE type = "table" AND name = "activity" ''')
-#
-# if c.fetchone()[0] == True:
-#     print("Table exists, skipping...")
-# else:
-#     # Create table and insert all current activites
-#     print("Generateing Table")
-#     c.execute(''' CREATE TABLE activity(id INTEGER PRIMARY KEY, name TEXT, run_datetime DATETIME)''')
-#     c.executemany(''' INSERT INTO activity(name, run_datetime) VALUES(?,?) ''', activity)
-#     db.commit()
-#
-# c.execute('''SELECT name, run_datetime FROM activity''')
-# for row in c:
-#     # Limit results to past 4 weeks (28 days)
-#     # This SHOULD prevent duplicates being uploaded to strava when the watch
-#     # automatically deletes old records
-#     if datetime.datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S") > (datetime.datetime.today() - datetime.timedelta(28)):
-#         if row[0] in os.listdir(fpath):
-#             print(f"{row[0]} {row[1]} already exists in records, skipping...")
-#         else:
-#             print("New workout found!")
-#             print(f"    Uploading: {row[0]} {row[1]} to Strava Servers (replace API code here)")
-#             print(f"    Saving:    {row[0]} {row[1]} to local database (replace code here)")
-#             print("    Success!")
-#
-# db.close()
-
+# Set up http header with users auth token, nothing special here
 headers = {
     'Authorization': f'Bearer {strava_token}',
 }
 
-# response = requests.get('https://www.strava.com/api/v3/athlete', headers=headers)
-#
-# for i in response.json():
-#     print(i, response.json()[i])
-
-# Past 28 days of activity
+# Now we check the Strava website to see what activites have already been
+# uploaded. If the activity has already been uploaded, "no" is changed to "yes"
 response = requests.get('https://www.strava.com/api/v3/athlete/activities', headers=headers)
 
 for i in response.json():
-    if datetime.datetime.strptime(i['start_date_local'], "%Y-%m-%dT%H:%M:%SZ") > (datetime.datetime.today() - datetime.timedelta(28)):
-        print(i['start_date_local'], i['external_id'])
+    for a in garmin_activity:
+        if a[0].lower() == i['external_id'].lower():
+            a.pop()
+            a.append('yes')
 
-# # Upload
-# files = {
-#     'file': ('7CI60334.FIT', open(f"{fpath}/7CI60334.FIT", 'rb')),
-#     'data_type': (None, 'fit'),
-# }
-#
-# response = requests.post('https://www.strava.com/api/v3/uploads', headers=headers, files=files)
+# Checks to see if 1) the activity is within a set range (see config file), and
+# checks to see if the activity is tagged with a "no", meaning is hasn't been
+# uploaded yet. NOTE: The day range that is set in the config file is a fail
+# safe so you don't accidentally upload really old workouts that were deleted
+# previously. The date range can be set to anything you want, but as long as you
+# plug your watch up to the computer after every workout, a range of 7 days is
+# probably pretty logical. Just be aware, if you set it to 7 (for example), but
+# don't plug your watch up till day 8, whatever workouts happened 8+ days ago
+# will not be uploaded.
+upload = []
+for i in garmin_activity:
+    if i[1] > (datetime.datetime.today() - datetime.timedelta(int(config['RANGE']['days']))) and i[2] == 'no':
+        upload.append(i)
+
+
+# Upload the files to Strava
+for i in upload:
+        print(i[0])
+        files = {
+            'file': (i[0], open(f"{fpath}/{i[0]}", 'rb')),
+            'data_type': (None, 'fit'),
+        }
+
+        response = requests.post('https://www.strava.com/api/v3/uploads', headers=headers, files=files)
